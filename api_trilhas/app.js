1113a514786a9e179019b4d560950607332e1957
GustavoCassimiro9 <gustavocassimiro60@gmail.com>
2022-11-27 16:08:21 -0300

    Upload e Delete de cadeiras

--- api_trilhas/app.js
+++ api_trilhas/app.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
-const jwt= require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 const cors = require('cors')
 const app = express()
 
@@ -21,41 +21,41 @@ const Cadeira = require('./models/Cadeiras');
 
 //Cadastrar Cadeiras
 
-app.post('/registroCadeira', cors(),  async (req,res) => {
+app.post('/registroCadeira', cors(), async (req, res) => {
 
-    const { Name, Trilha , Horario , Dia , Professor, Sobre } =  req.body
+    const { Name, Trilha, Horario, Dia, Professor, Sobre } = req.body
 
     let HorariosConta = 0;
 
-    console.log(Horario[0] , Horario[1])
-    HorariosConta = Math.abs( Horario[0] - Horario[1]) 
+    console.log(Horario[0], Horario[1])
+    HorariosConta = Math.abs(Horario[0] - Horario[1])
 
-  
-    
-    if(!Name){
-        return res.status(422).json( { msg: "O nome é obrigatório!"} )
-    }    
-    
-    if(!Trilha){
-        return res.status(422).json( { msg: "O Trilha é obrigatório!"} )
-    }
-    if(!Horario){
-        return res.status(422).json( { msg: "O Dia é obrigatória!"} )
-    }    
-    
-    if(!Dia){
-        return res.status(422).json( { msg: "O Dia é obrigatório!"} )
-    }
-    if(!Professor){
-        return res.status(422).json( { msg: "O nome do professor é obrigatório!"} )
-    }
-    
-    if(Dia.length > 1 &&  HorariosConta > 4 ){
 
-        return res.status(422).json( { msg: "A cadeira tem mais de quatro horas!"} )
+
+    if (!Name) {
+        return res.status(422).json({ msg: "O nome é obrigatório!" })
+    }
+
+    if (!Trilha) {
+        return res.status(422).json({ msg: "O Trilha é obrigatório!" })
+    }
+    if (!Horario) {
+        return res.status(422).json({ msg: "O Dia é obrigatória!" })
+    }
+
+    if (!Dia) {
+        return res.status(422).json({ msg: "O Dia é obrigatório!" })
+    }
+    if (!Professor) {
+        return res.status(422).json({ msg: "O nome do professor é obrigatório!" })
+    }
+
+    if (Dia.length > 1 && HorariosConta > 4) {
+
+        return res.status(422).json({ msg: "A cadeira tem mais de quatro horas!" })
 
     }
-    
+
 
 
     const cadeira = new Cadeira({
@@ -71,106 +71,162 @@ app.post('/registroCadeira', cors(),  async (req,res) => {
 
         await cadeira.save()
 
-        res.status(201).json({ msg: "Usuário criado com sucesso!"})
+        res.status(201).json({ msg: "Usuário criado com sucesso!" })
 
     } catch (error) {
 
         console.log(error)
 
-        res.status(500).json({ msg: "Aconteceu um erro no servidor, tente novamente ou mais tarde! "})
-        
+        res.status(500).json({ msg: "Aconteceu um erro no servidor, tente novamente ou mais tarde! " })
+
     }
 
 })
 
-app.get('/Cadeiras', cors(), async (req,res) => { 
+//Update de cadeiras
+app.patch('/cadeira/:id', async (req, res) => {
+    const id = req.params.id
+    const { Name, Trilha, Horario, Dia, Professor, Sobre } = req.body
+    const cadeira = {
+        Name,
+        Trilha,
+        Horario,
+        Dia,
+        Professor,
+        Sobre
+    }
+    try {
 
-        try{
+        const updateCadeira = await Cadeira.updateOne({ _id: id }, cadeira)
+        if (updateCadeira.matchedCount === 0) {
 
-            const cadeiras = await Cadeira.find();
-
-            res.status(200).json(cadeiras)
-
-
-        }catch(error){
-            res.status(500).json({ error: error })
+            res.status(422).json({ message: 'Cadeira não encontrada' })
+            return
 
         }
+        res.status(200).json(cadeira)
+
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+
+
 })
 
+//Chamar cadeiras
+app.get('/cadeiras', cors(), async (req, res) => {
+
+    try {
+
+        const cadeiras = await Cadeira.find();
+
+        res.status(200).json(cadeiras)
+
+
+    } catch (error) {
+        res.status(500).json({ error: error })
+
+    }
+})
+
+//Deletar cadeiras
+app.delete('cadeira/:id', async (req, res) => {
+
+    const id = req.params.id
+
+    const cadeira = await Cadeira.findOne({ _id: id });
+
+    if (!cadeira) {
+
+        res.status(422).json({ message: 'Cadeira não encontrada' })
+        return
+
+    }
+
+    try {
+
+        await Cadeira.deleteOne({ _id: id })
+        res.status(200).json({ message: 'Cadeira removida com sucesso!' })
+
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+
+
+})
 
 // Public Route
-app.get('/', cors(), (req,res) => { 
+app.get('/', cors(), (req, res) => {
     res.status(200).json({
-        msg:"Bem Vindo a nossa API!",
-        status:true
+        msg: "Bem Vindo a nossa API!",
+        status: true
     })
 })
 
 // Private Route
-app.get('/user/:id', checkToken ,async (req,res) => {
+app.get('/user/:id', checkToken, async (req, res) => {
 
     const id = req.params.id
-    
+
     //check if user exists
     const user = await User.findById(id, '-password')
 
-    if(!user){
-        return res.status(404).json({ msg: "Usuário não encontrado" }) 
+    if (!user) {
+        return res.status(404).json({ msg: "Usuário não encontrado" })
     }
 
     res.status(200).json({ user })
 })
 
-function checkToken(req, res, next){
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(" ")[1]
+function checkToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
 
-  if(!token){
-    return res.status(401).json({ msg: "Acesso Negado!"})
-  }
+    if (!token) {
+        return res.status(401).json({ msg: "Acesso Negado!" })
+    }
 
-  try {
-    
-    const secret = process.env.SECRET
+    try {
 
-    jwt.verify(token, secret)
+        const secret = process.env.SECRET
 
-    next()
+        jwt.verify(token, secret)
 
-  } catch (error) {
-    res.status(400).json({ msg : "Token Inválido!"})
-  }
+        next()
+
+    } catch (error) {
+        res.status(400).json({ msg: "Token Inválido!" })
+    }
 
 }
 
 // Register User
-app.post('/auth/register', cors(),  async (req,res) => {
+app.post('/auth/register', cors(), async (req, res) => {
 
-    const { name, email , password , confirmPassword} =  req.body
+    const { name, email, password, confirmPassword } = req.body
 
-    if(!name){
-        return res.status(422).json( { msg: "O nome é obrigatório!"} )
-    }    
-    
-    if(!email){
-        return res.status(422).json( { msg: "O email é obrigatório!"} )
-    }    
-    
-    if(!password){
-        return res.status(422).json( { msg: "A senha é obrigatória!"} )
+    if (!name) {
+        return res.status(422).json({ msg: "O nome é obrigatório!" })
     }
 
-    if(password !== confirmPassword){
-        return res.status(422).json( { msg: "As senhas não coincidem!"} )
+    if (!email) {
+        return res.status(422).json({ msg: "O email é obrigatório!" })
+    }
+
+    if (!password) {
+        return res.status(422).json({ msg: "A senha é obrigatória!" })
+    }
+
+    if (password !== confirmPassword) {
+        return res.status(422).json({ msg: "As senhas não coincidem!" })
     }
 
 
     //check if user exists
-    const userExists = await User.findOne( { email: email} )
+    const userExists = await User.findOne({ email: email })
 
-    if(userExists){
-        return res.status(422).json({ msg:`Email ja registrado. Por favor, utilize outro e-mail!`})
+    if (userExists) {
+        return res.status(422).json({ msg: `Email ja registrado. Por favor, utilize outro e-mail!` })
     }
 
     // create password
@@ -180,50 +236,50 @@ app.post('/auth/register', cors(),  async (req,res) => {
     const user = new User({
         name,
         email,
-        password:passwordHash
+        password: passwordHash
     })
 
     try {
 
         await user.save()
 
-        res.status(201).json({ msg: "Usuário criado com sucesso!"})
+        res.status(201).json({ msg: "Usuário criado com sucesso!" })
 
     } catch (error) {
 
         console.log(error)
 
-        res.status(500).json({ msg: "Aconteceu um erro no servidor, tente novamente ou mais tarde! "})
-        
+        res.status(500).json({ msg: "Aconteceu um erro no servidor, tente novamente ou mais tarde! " })
+
     }
 
 })
 
 // Login User
-app.post("/auth/login", async (req,res) => {
-    const { email, password } = req.body 
+app.post("/auth/login", async (req, res) => {
+    const { email, password } = req.body
 
-    if(!email){
-        return res.status(422).json( { msg: "O email é obrigatório!"} )
-    }    
-    
-    if(!password){
-        return res.status(422).json( { msg: "A senha é obrigatória!"} )
+    if (!email) {
+        return res.status(422).json({ msg: "O email é obrigatório!" })
+    }
+
+    if (!password) {
+        return res.status(422).json({ msg: "A senha é obrigatória!" })
     }
 
     //check if user exists
-    const user = await User.findOne( { email: email} )
+    const user = await User.findOne({ email: email })
 
-    if(!user){
-        return res.status(422).json({ msg:`Usuário não encontrado!`})
+    if (!user) {
+        return res.status(422).json({ msg: `Usuário não encontrado!` })
     }
 
     //check if password match
 
     const checkPassword = await bcrypt.compare(password, user.password)
 
-    if(!checkPassword){
-        return res.status(422).json({ msg:`Senha inválida!`})
+    if (!checkPassword) {
+        return res.status(422).json({ msg: `Senha inválida!` })
     }
 
     try {
@@ -232,25 +288,25 @@ app.post("/auth/login", async (req,res) => {
 
         const token = jwt.sign(
             {
-                id:user.id,
+                id: user.id,
             },
             secret
         )
 
         res.status(200).json({
             msg: "Autenticação realizada com sucesso", token,
-            user: { 
+            user: {
                 username: user.name,
                 user_id: user.id,
                 email: user.email
             },
-            status:true
-         })
-            
+            status: true
+        })
+
     } catch (error) {
         console.log(error)
 
-        return res.status(500).json({ msg:`Aconteceu um erro no servidor, tente novamente ou mais tarde!`})
+        return res.status(500).json({ msg: `Aconteceu um erro no servidor, tente novamente ou mais tarde!` })
 
     }
 
@@ -260,8 +316,8 @@ app.post("/auth/login", async (req,res) => {
 
 
 // Credentials
-const dbUser =  process.env.DB_USER
-const dbPass =  process.env.DB_PASS
+const dbUser = process.env.DB_USER
+const dbPass = process.env.DB_PASS
 
 // DB Connection + Start API 
 mongoose.connect(
